16ea18d4abb33a27df013c6d154d3ba15f0bf551
Frank Jungblut <franksjungblut@gmail.com>
2024-03-17 21:29:38 -0300

    primeira parte feita

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 const contagem = document.querySelector(".contagem")
+let cron
+let segundos = 0
+let minutos = 0
+let horas = 0
 
 function iniciar() {
-    let centesimos = 0
-    let segundos = 0
-    let minutos = 0
-    let horas = 0
-    
-    setInterval(() => {
-        centesimos++
+    cron = setInterval(() => {
 
-        if (centesimos == 100) {
-            centesimos = 0
-            segundos++
-        }
+        segundos++
 
         if (segundos == 60) {
             segundos = 0
@@ -27,9 +22,23 @@ function iniciar() {
         let horasFormatadas = horas.toString().padStart(2, "0");
         let minutosFormatados = minutos.toString().padStart(2, "0");
         let segundosFormatados = segundos.toString().padStart(2, "0");
-        let centesimosFormatados = centesimos.toString().padStart(2, "0");
 
-        contagem.value = `${horasFormatadas}:${minutosFormatados}:${segundosFormatados}.${centesimosFormatados}`;
+        contagem.value = `${horasFormatadas}:${minutosFormatados}:${segundosFormatados}`;
 
-    }, 10);
+    }, 1000);
+}
+
+function parar() {
+    clearInterval(cron)
+}
+
+function zerar() {
+    segundos = 0
+    minutos = 0
+    horas = 0
+    contagem.value = "00:00:00"
+}
+
+function voltas() {
+    
 }
